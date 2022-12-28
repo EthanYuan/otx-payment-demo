3d51b277fc5b0f4c048316d56a90be4e1272a7e6
EthanYuan <yuanchengxing@hotmail.com>
2022-12-28 15:03:18 +0800

    re-generate opentx types.

--- open-tx/src/lib.rs
+++ open-tx/src/lib.rs
@@ -4,6 +4,8 @@ pub use generated::packed;
 
 #[cfg(test)]
 mod test {
+    use crate::packed::OpenTransaction;
+
     use super::*;
     use ckb_jsonrpc_types::JsonBytes;
     use generated::packed::OpenTransactionBuilder;
@@ -13,8 +15,15 @@ mod test {
     fn test_serialize() {
         let builder = OpenTransactionBuilder::default();
         let opentx = builder.build();
-        let json_rpc_format = JsonBytes::from_bytes(opentx.as_bytes());
+        let opentx_bytes = opentx.as_bytes();
+        let json_rpc_format = JsonBytes::from_bytes(opentx_bytes);
         println!("{:?}", opentx);
         println!("{:?}", json_rpc_format);
+
+        let opentx_bytes = json_rpc_format.as_bytes();
+        println!("{:?}", opentx_bytes);
+        let opentx_rebuild = OpenTransaction::from_slice(opentx_bytes).unwrap();
+
+        assert_eq!(opentx.as_bytes(), opentx_rebuild.as_bytes())
     }
 }
