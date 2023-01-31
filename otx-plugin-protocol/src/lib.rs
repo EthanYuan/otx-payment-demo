327e39f982a8306c0342ef9a635c1bd8b15e291e
EthanYuan <yuanchengxing@hotmail.com>
2023-02-01 00:08:20 +0800

    add otx plugin protocol.

--- otx-plugin-protocol/src/lib.rs
+++ otx-plugin-protocol/src/lib.rs
@@ -1,7 +1,41 @@
+use otx_format::jsonrpc_types::OpenTransaction;
+
 use serde_derive::{Deserialize, Serialize};
 
+pub type Id = u64;
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct PluginConfig {
+pub struct PluginInfo {
     pub name: String,
     pub description: String,
+    pub version: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum MessageFromHost {
+    // Notify
+    NewOtx((Id, OpenTransaction)),
+    NewInterval,
+    OtxPoolStart,
+    OtxPoolStop,
+    DeleteOtx(Id),
+
+    // Request
+    GetPluginInfo,
+    // Response
+}
+
+pub enum MessageFromPlugin {
+    // Notify
+
+    // Response
+    Ok,
+    Error(String),
+    PluginInfo(PluginInfo),
+
+    // Request
+    NewOtx(OpenTransaction),
+    DiscardOtx(Id),
+    ModifyOtx((Id, OpenTransaction)),
+    SendCkbTx(OpenTransaction),
 }
